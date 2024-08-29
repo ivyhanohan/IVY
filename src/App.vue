87f4ad2a9da4f6f86eeaf12dbe4a495151114576
ivyhanohan <ivyhanohan@gmail.com>
2024-08-29 09:55:23 +0800

    quino

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+
+<template>
+  <div>
+    <h2>Active List</h2>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item.name }}
+        <span v-if="item.isActive">(Active)</span>
+        <span class="nigga" v-else>(Offline)</span>
+      </li>
+    </ul>
+    <p>Total Active Person: {{ activeItemCount }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { id: 1, name: "Harvey Quibol", isActive: true },
+        { id: 2, name: "Ivy Quino", isActive: true },
+        { id: 3, name: "Jeric Polison", isActive: false },
+        { id: 4, name: "Jerico Pasana", isActive: true },
+      ],
+    };
+  },
+  computed: {
+    filteredItems() {
+      return this.items.filter(item => item.isActive);
+    },
+    activeItemCount() {
+      return this.filteredItems.length;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+  margin-left: 90px;
+  margin-top: 50px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin-left: 70px;
+  
+}
+li {
+  margin: 5px 0;
+  
+}
+span {
+  color: rgb(112, 206, 112);
+  font-weight: bold;
+}
+div {
+  border: 1px solid black;
+  height: 300px;
+  width: 300px;
+  border-radius: 20px;
+  justify-content: center;
+}
+p{
+  margin-left: 70px;
+  margin-top: 10px;
+}
+.nigga{
+  color: white;
+}
+
+</style>
---
+++ src/components/Loop.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Total Price: ${{ totalPrice }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        items: [
+          { name: 'Laptop', price: 1000 },
+          { name: 'Phone', price: 500 },
+          { name: 'Tablet', price: 300 }
+        ]
+      };
+    },
+    computed: {
+      // A computed property to calculate the total price of all items
+      totalPrice() {
+        return this.items.reduce((sum, item) => sum + item.price, 0);
+      }
+    }
+  };
+  </script>
+  
+  create a web application that demonstrate how to loop aover arrays, conditionally display HTML and prepare computed properties
